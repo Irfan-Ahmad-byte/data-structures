1b7ed5a6d90c6d3062f6d46b8e577151aff52b96
Irfan-Ahmad-byte <irfan.ahmad.mlka@gmail.com>
2022-05-18 13:42:59 +0500

    arrays

---
+++ arrays.cpp
@@ -0,0 +1,14 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+
+int main() {
+   int x[6];
+
+   int j;
+
+   for(j = 0; j < 6; j++){
+       x[j] = 2 * j;
+       std::cout << j;
+   }
+}
+
---
+++ arrays.py
@@ -0,0 +1,9 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+import array
+
+a = array.array('i', [1,2,3,4])
+# b = int(input('Enter 2nd number: '))
+for i in a:
+    print(i)
+
